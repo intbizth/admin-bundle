015123062e713a6b3a42c490ef14e6790b9f4e02
ツ Liverbool <nukboon@gmail.com>
2016-05-11 19:56:55 +0700

    prepend grid extension.

--- DependencyInjection/IntbizthAdminExtension.php
+++ DependencyInjection/IntbizthAdminExtension.php
@@ -4,6 +4,7 @@ namespace Intbizth\Bundle\AdminBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -12,7 +13,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class IntbizthAdminExtension extends Extension
+class IntbizthAdminExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -24,5 +25,29 @@ class IntbizthAdminExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $this->processConfiguration(new Configuration(), $container->getExtensionConfig($this->getAlias()));
+
+        $container->prependExtensionConfig('sylius_grid', array(
+            'templates' => array(
+                'action' => array(
+                    'create' => 'IntbizthAdminBundle:Grid/Action:create.html.twig',
+                    'update' => 'IntbizthAdminBundle:Grid/Action:update.html.twig',
+                    'delete' => 'IntbizthAdminBundle:Grid/Action:delete.html.twig',
+                    'link' => 'IntbizthAdminBundle:Grid/Action:link.html.twig',
+                    'show' => 'IntbizthAdminBundle:Grid/Action:show.html.twig',
+                    'route' => 'IntbizthAdminBundle:Grid/Action:route.html.twig',
+                ),
+                'filter' => array(
+                    'string' => 'IntbizthAdminBundle:Grid/Filter:string.html.twig',
+                ),
+            )
+        ));
     }
 }
