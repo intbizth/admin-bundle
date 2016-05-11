6c76541d5b8b22d22f5cc838c03985f5f39d7086
ツ Liverbool <nukboon@gmail.com>
2016-05-11 18:45:30 +0700

    override ui-bundle

--- IntbizthAdminBundle.php
+++ IntbizthAdminBundle.php
@@ -6,4 +6,11 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class IntbizthAdminBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'SyliusUiBundle';
+    }
 }
